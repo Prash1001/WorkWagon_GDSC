77e2d547b2ec0e6f26edd93cd2352d1a30b749d3
Prashant Patil <109451910+Prash1001@users.noreply.github.com>
2024-02-25 21:02:24 +0530

    update

--- client/src/firebaseapp.js
+++ client/src/firebaseapp.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBqowirI_OjJJPUA120hnYbkIjllp1Ntrg",
-  authDomain: "workwagon-ea1bc.firebaseapp.com",
-  projectId: "workwagon-ea1bc",
-  storageBucket: "workwagon-ea1bc.appspot.com",
-  messagingSenderId: "376854047650",
-  appId: "1:376854047650:web:9faf53772525cb4add60fb",
-  measurementId: "G-HH496MDY4N",
+  apiKey: "//",
+  authDomain: "//",
+  projectId: "//",
+  storageBucket: "//",
+  messagingSenderId: "//",
+  appId: "//",
+  measurementId: "//",
 };
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
